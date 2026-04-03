2ccf0386475288bd58bb77577f0e79248aaa23b0
u.petrov <u.petrov@skbkontir.ru>
2026-04-03 14:50:33 +0500

    fix

--- script.sh
+++ script.sh
@@ -241,7 +241,7 @@ if [ "$1" = "-update" ]; then
 
     curl -s -o /opt/etc/bot.py https://raw.githubusercontent.com/${repo}/bypass_keenetic/main/bot.py
     chmod 755 /opt/etc/bot.py
-    curl -o /opt/etc/init.d/S101web https://raw.githubusercontent.com/${repo}/bypass_keenetic/main/S101web
+    curl -s -o /opt/etc/init.d/S101web https://raw.githubusercontent.com/${repo}/bypass_keenetic/main/S101web
     chmod 755 /opt/etc/init.d/S101web || chmod +x /opt/etc/init.d/S101web
     mkdir -p /opt/root/www/
     curl -s -o /opt/root/www/index.html https://raw.githubusercontent.com/${repo}/bypass_keenetic/main/index.html
